f15ef02a09e3bd39f1e9fd6b0c2114c7ccbf335b
Dwake5 <wakeling101@hotmail.com>
2019-04-05 18:26:31 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  #This is the crux of the anagram checker, it takes each element of the potential
+  #Matching array and breaks it down into letters and sorts them alphabetically
+  #It does the same for the given word. If these two are the same it returns true
+  def match(array)
+    array.select {|x| x.split("").sort == @word.split("").sort}
+  end
+end
